36338e1d009e52726aab06c419e5a88fa1cd174a
Your Name <you@example.com>
2021-06-14 20:44:35 +0530

    Books ui added in front end

--- src/app/modules/books/books.module.ts
+++ src/app/modules/books/books.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BookInfoComponent } from './book-info/book-info.component';
 import { BooksListComponent } from './books-list/books-list.component';
 import { BooksRoutes } from './books.routing';
 
-const COMPONENTS = [BooksListComponent];
+const COMPONENTS = [BooksListComponent, BookInfoComponent];
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [BooksRoutes],
+  imports: [BooksRoutes, FormsModule],
   providers: [],
 })
 export class BooksModule {}
--- src/app/modules/books/books.routing.ts
+++ src/app/modules/books/books.routing.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
+import { BookInfoComponent } from './book-info/book-info.component';
 import { BooksListComponent } from './books-list/books-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BooksListComponent,
+  },
+  {
+    path: ':id',
+    component: BookInfoComponent,
   },
 ];
 
