b1364331124490210d3a288fad5bf15dbc2b1cd8
Your Name <you@example.com>
2021-06-28 18:17:35 +0530

    project builded

--- server.js
+++ server.js
@@ -38,13 +38,6 @@ Server.prototype.initExpressMiddleWare = () => {
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     })
   );
-
-  // app.use((req, res, next) => {
-  //   res.append("Access-Control-Allow-Origin", ["*"]);
-  //   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  //   res.append("Access-Control-Allow-Headers", "Content-Type");
-  //   next();
-  // });
 };
 
 // Initialize MongoDB
